676e2914b5fedc643f81474fdd88aae7d9574fc3
srirangaraj-b <sreerangaraj387@gmail.com>
2026-07-04 18:36:48 +0530

    acessing with custom device tree and with GPIOG

--- gpio-access/src/main.c
+++ gpio-access/src/main.c
@@ -3,27 +3,26 @@
 
 #define LED_NODEA DT_ALIAS(leda)
 #define LED_NODEB DT_ALIAS(ledb)
-#define LED_NODEC DT_ALIAS(ledc)
 
 const struct gpio_dt_spec leda = GPIO_DT_SPEC_GET(LED_NODEA, gpios);
 const struct gpio_dt_spec ledb = GPIO_DT_SPEC_GET(LED_NODEB, gpios);
-const struct gpio_dt_spec ledc = GPIO_DT_SPEC_GET(LED_NODEC, gpios);
 
-int main(){
+const struct device *gpiog = DEVICE_DT_GET(DT_NODELABEL(gpiog)); //getting the GPIOG from the device tree
 
-    printk("Program with Shriiii!!!!");
-    printk("LED Flashing Program");
-    
+int main(void)
+{
     gpio_pin_configure_dt(&leda, GPIO_OUTPUT_HIGH);
     gpio_pin_configure_dt(&ledb, GPIO_OUTPUT_HIGH);
-    gpio_pin_configure_dt(&ledc, GPIO_OUTPUT_HIGH);
 
-    while(1){
+    gpio_pin_configure(gpiog, 10, GPIO_OUTPUT_ACTIVE); //since we dont use the device tree we dont have to call the function
+
+    while (1) {
         gpio_pin_toggle_dt(&leda);
         gpio_pin_toggle_dt(&ledb);
-        gpio_pin_toggle_dt(&ledc);
-        k_msleep(500);
 
+        gpio_pin_toggle(gpiog, 10); //Directly call the toggling from the GPIO Pin accessing
+
+        k_msleep(500);
     }
 
     return 0;
